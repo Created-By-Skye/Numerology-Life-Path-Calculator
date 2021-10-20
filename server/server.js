c633d3065e2ab32febae5b7f0446b81f49ddc2d9
Brandon Keefer <happysolucki@outlook.com>
2021-10-20 01:57:55 -0500

    set up routes for future seeding for profiles and numerology info

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const seedProfilesRouter = require("./routes/seed-profiles.js");
+const seedNumerologyRouter = require("./routes/seed-numerology.js");
 
 mongoose.connect(process.env.DATABASE_URL, {});
 mongoose.connection.on(
@@ -18,5 +20,8 @@ const PORT = process.env.PORT || 3001;
 app.get("/test", (_, res) => {
   res.send("test request received");
 });
+
+app.use("/seed/profiles", seedProfilesRouter);
+app.use("/seed/numerology", seedNumerologyRouter);
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
