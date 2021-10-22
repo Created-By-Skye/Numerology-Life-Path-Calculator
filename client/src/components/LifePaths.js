810dc17b450b20ae811ea370adf9ec66789cd79d
Brandon Keefer <happysolucki@outlook.com>
2021-10-21 21:10:19 -0500

    fetched life path data from backend; added placeholder display of lifepath titles

--- client/src/components/LifePaths.js
+++ client/src/components/LifePaths.js
@@ -1,11 +1,55 @@
-import React from 'react';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const LifePaths = () => {
+  const [lifePaths, setLifePaths] = useState([]);
+  const [error, setError] = useState(undefined);
 
+  // const fetchLifePaths = async () => {
+  //   try {
+  //     const { data } = await axios.get("http://localhost:3001/numerology");
+  //     setLifePaths(data);
+  //     error && setError(undefined);
+  //   } catch (err) {
+  //     setError({
+  //       status: err.response.status,
+  //       errMsg: "Couldn't fetch life paths from db",
+  //     });
+  //   }
+  // };
 
-return (
-  <h1>Life Paths</h1>
-)
-}
+  useEffect(() => {
+    const fetchLifePaths = async () => {
+      try {
+        const { data } = await axios.get("http://localhost:3001/numerology");
+        setLifePaths(data);
+        error && setError(undefined);
+      } catch (err) {
+        setError({
+          status: err.response.status,
+          errMsg: "Couldn't fetch life paths from db",
+        });
+      }
+    };
+    fetchLifePaths();
+  }, [error]);
 
-export default LifePaths
+  if (lifePaths.length < 1) {
+    return <div>Loading...</div>;
+  } else if (error) {
+    return <div>Failed to fetch life paths</div>;
+  }
+
+  return (
+    <>
+      <h1>Life Paths</h1>
+      <div>
+        {lifePaths.map((path, idx) => (
+          <h1 key={idx}>{path.title}</h1>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default LifePaths;
